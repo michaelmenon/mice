9f7042b270d7e7dc1fc3cbc06cb71f7113bb8ab8
Michael <michael.menon@zco.com>
2021-12-18 23:30:22 -0500

    log fatal added to run

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"flag"
+	"fmt"
+	"log"
 	"mice/router"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -33,11 +35,12 @@ func main(){
 	addr := fmt.Sprintf("%s:%s",server["ip"],server["port"])
 	
 	if viper.GetBool("config.tls"){
+
 		fmt.Printf("Mice [INFO]:Running the Mice Gateway over TLS on => %s\n",addr)
-		gr.RunTLS(addr,viper.GetString("config.cert"),viper.GetString("config.key"))
+		log.Fatal(gr.RunTLS(addr,viper.GetString("config.cert"),viper.GetString("config.key")))
 	}else{
 		fmt.Printf("Mice [INFO]:Running the Mice Gateway on => %s\n",addr)
-		gr.Run(addr)
+		log.Fatal(gr.Run(addr))
 	}
 
 }
